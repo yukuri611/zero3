b23ed75746e8dba7e2cf394ee4ab551d1491b6fc
yukuri611 <yukiiw611@gmail.com>
2025-12-14 16:06:35 +0900

    implement backprop. using multi input

---
+++ steps/step13.py
@@ -0,0 +1,110 @@
+import numpy as np
+
+class Variable:
+    def __init__(self, data):
+        if data is not None:
+            if not isinstance(data, np.ndarray):
+                raise TypeError("{} is not supported".format(type(data)))
+        self.data = data
+        self.grad = None
+        self.creator = None
+    
+    def set_creator(self, func):
+        self.creator = func
+    
+    def backward(self):
+        if self.grad is None:
+            self.grad = np.ones_like(self.data)
+
+        funcs = [self.creator]
+        while funcs:
+            f = funcs.pop()
+            gys = [output.grad for output in f.outputs]
+            gxs = f.backward(*gys)
+            if not isinstance(gxs, tuple):
+                gxs = (gxs,)
+            
+            for x, gx in zip(f.inputs, gxs):
+                x.grad = gx
+                if x.creator is not None:
+                    funcs.append(x.creator)
+
+class Function:
+    def __call__(self, *inputs):
+        xs = [x.data for x in inputs]
+        ys = self.forward(*xs)
+        if not isinstance(ys, tuple):
+            ys = (ys,)
+
+        outputs = [Variable(as_array(y)) for y in ys]
+        for output in outputs:
+            output.set_creator(self)
+        self.inputs = inputs
+        self.outputs = outputs
+        return outputs if len(outputs) > 1 else outputs[0]
+
+    def forward(self, xs):
+        raise NotImplementedError()
+
+    def backward(self, gys):
+        raise NotImplementedError()
+    
+class Add(Function) :
+    def forward(self, x0, x1):
+        y = x0 + x1
+        return y
+    def backward(self, gy):
+        return (gy, gy)
+    
+  
+class Square(Function):
+    def forward(self, x):
+        y = x ** 2
+        return  y
+    def backward(self, gy):
+        x = self.inputs[0].data
+        gx = 2 * x * gy
+        return gx
+
+    
+class Exp(Function):
+    def forward(self, x):
+        return np.exp(x)
+    def backward(self, gy):
+        x = self.input.data
+        gx = np.exp(x) * gy
+        return gx
+
+  
+def numerical_diff(f, x, eps=1e-4):
+        x0 = Variable(x.data - eps)
+        x1 = Variable(x.data + eps)
+        y0 = f(x0)
+        y1 = f(x1)
+        return (y1.data - y0.data) / (2 * eps)
+
+def square(x):
+    f = Square()
+    return f(x)
+
+def exp(x):
+    f = Exp()
+    return f(x)
+
+def add(x, y):
+    f = Add()
+    return f(x, y)
+
+
+def as_array(x):
+    if np.isscalar(x):
+        return np.array(x)
+    return x
+
+x = Variable(np.array(2.0))
+y = Variable(np.array(3.0))
+z = add(square(x), square(y))
+z.backward()
+print(z.data)
+print(x.grad)
+print(y.grad)
