468aa6fdcc6c8995d4d49d1de8972b76cd377b57
yukuri611 <yukiiw611@gmail.com>
2025-12-14 15:39:04 +0900

    handle multidimention input&output

---
+++ steps/step11.py
@@ -0,0 +1,94 @@
+import numpy as np
+
+class Variable:
+    def __init__(self, data):
+        if data is not None:
+            if not isinstance(data, np.ndarray):
+                raise TypeError("{} is not supported".format(type(data)))
+        self.data = data
+        self.grad = None
+        self.creator = None
+    
+    def set_creator(self, func):
+        self.creator = func
+    
+    def backward(self):
+        if self.grad is None:
+            self.grad = np.ones_like(self.data)
+
+        funcs = [self.creator]
+        while funcs:
+            f = funcs.pop()
+            x, y = f.input, f.output
+            x.grad = f.backward(y.grad)
+            if x.creator is not None:
+                funcs.append(x.creator)
+
+class Function:
+    def __call__(self, inputs):
+        xs = [x.data for x in inputs]
+        ys = self.forward(xs)
+        outputs = [Variable(as_array(y)) for y in ys]
+        for output in outputs:
+            output.set_creator(self)
+        self.inputs = inputs
+        self.outputs = outputs
+        return outputs
+
+    def forward(self, xs):
+        raise NotImplementedError()
+
+    def backward(self, gys):
+        raise NotImplementedError()
+    
+class Add(Function) :
+    def forward(self, xs):
+        x0, x1 = xs
+        y = x0 + x1
+        return (y,)
+    
+  
+class Square(Function):
+    def forward(self, x):
+        y = x ** 2
+        return  y
+    def backward(self, gy):
+        x = self.input.data
+        gx = 2 * x * gy
+        return gx
+
+    
+class Exp(Function):
+    def forward(self, x):
+        return np.exp(x)
+    def backward(self, gy):
+        x = self.input.data
+        gx = np.exp(x) * gy
+        return gx
+
+  
+def numerical_diff(f, x, eps=1e-4):
+        x0 = Variable(x.data - eps)
+        x1 = Variable(x.data + eps)
+        y0 = f(x0)
+        y1 = f(x1)
+        return (y1.data - y0.data) / (2 * eps)
+
+def square(x):
+    f = Square()
+    return f(x)
+
+def exp(x):
+    f = Exp()
+    return f(x)
+
+def as_array(x):
+    if np.isscalar(x):
+        return np.array(x)
+    return x
+
+xs = [Variable(np.array(2)), Variable(np.array(3))]
+f = Add()
+ys = f(xs)
+y = ys[0]
+print(y.data)
