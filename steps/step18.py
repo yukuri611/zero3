b47c791e91b44d57a05ab05962ae43f90f18d3ee
yukuri611 <yukiiw611@gmail.com>
2025-12-15 19:46:34 +0900

    add only forward mode

---
+++ steps/step18.py
@@ -0,0 +1,153 @@
+import numpy as np
+import heapq
+import weakref
+import contextlib
+
+class Variable:
+    def __init__(self, data):
+        if data is not None:
+            if not isinstance(data, np.ndarray):
+                raise TypeError("{} is not supported".format(type(data)))
+        self.data = data
+        self.grad = None
+        self.creator = None
+        self.generation = 0
+    
+    def set_creator(self, func):
+        self.creator = func
+        self.generation = func.generation + 1
+    
+    def backward(self, retain_grad=False):
+        if self.grad is None:
+            self.grad = np.ones_like(self.data)
+
+        funcs = []
+        seen_set = set()
+        def add_func(f):
+            if f not in seen_set:
+                seen_set.add(f)
+                heapq.heappush(funcs, (-f.generation, f))
+
+        add_func(self.creator)
+        while funcs:
+            generation, f = heapq.heappop(funcs)
+            gys = [output().grad for output in f.outputs]
+            gxs = f.backward(*gys)
+            if not isinstance(gxs, tuple):
+                gxs = (gxs,)
+            
+            for x, gx in zip(f.inputs, gxs):
+                if x.grad is None:
+                    x.grad= gx
+                else:
+                    x.grad = x.grad + gx
+                if x.creator is not None:
+                    add_func(x.creator)
+            if not retain_grad:
+                for y in f.outputs:
+                    y().grad = None
+    
+    def cleargrad(self):
+        self.grad = None
+
+class Function:
+    def __lt__(self, other):
+        return self.generation < other.generation
+    def __call__(self, *inputs):
+        xs = [x.data for x in inputs]
+        ys = self.forward(*xs)
+        if not isinstance(ys, tuple):
+            ys = (ys,)
+        outputs = [Variable(as_array(y)) for y in ys]
+
+        if Config.enable_backprop:
+            self.generation = max([x.generation for x in inputs])
+            for output in outputs:
+                output.set_creator(self)
+            self.inputs = inputs
+            self.outputs = [weakref.ref(output) for output in outputs]
+        return outputs if len(outputs) > 1 else outputs[0]
+
+    def forward(self, xs):
+        raise NotImplementedError()
+
+    def backward(self, gys):
+        raise NotImplementedError()
+
+class Config:
+    enable_backprop = True
+
+@contextlib.contextmanager
+def using_config(name, value):
+    old_value = getattr(Config, name)
+    setattr(Config, name, value)
+    try:
+        yield
+    finally:
+        setattr(Config, name, old_value)
+
+def no_grad():
+    return using_config('enable_backprop', False)
+
+
+    
+class Add(Function) :
+    def forward(self, x0, x1):
+        y = x0 + x1
+        return y
+    def backward(self, gy):
+        return (gy, gy)
+    
+  
+class Square(Function):
+    def forward(self, x):
+        y = x ** 2
+        return  y
+    def backward(self, gy):
+        x = self.inputs[0].data
+        gx = 2 * x * gy
+        return gx
+
+    
+class Exp(Function):
+    def forward(self, x):
+        return np.exp(x)
+    def backward(self, gy):
+        x = self.input.data
+        gx = np.exp(x) * gy
+        return gx
+
+  
+def numerical_diff(f, x, eps=1e-4):
+        x0 = Variable(x.data - eps)
+        x1 = Variable(x.data + eps)
+        y0 = f(x0)
+        y1 = f(x1)
+        return (y1.data - y0.data) / (2 * eps)
+
+def square(x):
+    f = Square()
+    return f(x)
+
+def exp(x):
+    f = Exp()
+    return f(x)
+
+def add(x, y):
+    f = Add()
+    return f(x, y)
+
+
+def as_array(x):
+    if np.isscalar(x):
+        return np.array(x)
+    return x
+
+
+with using_config('enable_backprop', False):
+    x = Variable(np.array(2.0))
+    y = square(x)
+
+with no_grad():
+    x = Variable(np.array(2.0))
+    y = square(x)
