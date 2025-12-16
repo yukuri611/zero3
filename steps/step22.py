5dc6adf972b1a9659c16e559ac6b150e4c104392
yukuri611 <yukiiw611@gmail.com>
2025-12-16 16:04:31 +0900

    overloaded division

--- steps/step22.py
+++ steps/step22.py
@@ -270,6 +270,32 @@ def pow(x, c):
 
 Variable.__pow__ = pow
 
-x = Variable(np.array(2.0))
-y = x**3
+
+class Div(Function):
+    def forward(self, x0, x1):
+        y = x0 / x1
+        return y
+
+    def backward(self, gy):
+        x0, x1 = self.inputs[0].data, self.inputs[1].data
+        gx0 = gy / x1
+        gx1 = gy * (-x0 / x1**2)
+        return gx0, gx1
+
+
+def div(x0, x1):
+    x1 = as_array(x1)
+    return Div()(x0, x1)
+
+
+def rdiv(x0, x1):
+    x1 = as_array(x1)
+    return Div()(x1, x0)
+
+
+Variable.__truediv__ = div
+Variable.__rtruediv__ = rdiv
+
+x = Variable(np.array(7.0))
+y = x / 2
 print(y)
