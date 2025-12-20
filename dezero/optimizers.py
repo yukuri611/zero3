0a1153809302d6d9eacd2361ae5e7143b55b5cf7
yukuri611 <yukiiw611@gmail.com>
2025-12-20 16:26:13 +0900

    implement relu and adam

--- dezero/optimizers.py
+++ dezero/optimizers.py
@@ -1,3 +1,5 @@
+import math
+
 import numpy as np
 
 
@@ -49,3 +51,39 @@ class MomemtumSGD(Optimizer):
         v *= self.momentum
         v -= self.lr * param.grad.data
         param.data += v
+
+
+class Adam(Optimizer):
+    def __init__(self, alpha=0.001, beta1=0.9, beta2=0.999, eps=1e-8):
+        super().__init__()
+        self.t = 0
+        self.alpha = alpha
+        self.beta1 = beta1
+        self.beta2 = beta2
+        self.eps = eps
+        self.ms = {}
+        self.vs = {}
+
+    def update(self, *args, **kwargs):
+        self.t += 1
+        super().update(*args, **kwargs)
+
+    @property
+    def lr(self):
+        fix1 = 1.0 - math.pow(self.beta1, self.t)
+        fix2 = 1.0 - math.pow(self.beta2, self.t)
+        return self.alpha * math.sqrt(fix2) / fix1
+
+    def update_one(self, param):
+        key = id(param)
+        if key not in self.ms:
+            self.ms[key] = np.zeros_like(param.data)
+            self.vs[key] = np.zeros_like(param.data)
+
+        m, v = self.ms[key], self.vs[key]
+        beta1, beta2, eps = self.beta1, self.beta2, self.eps
+        grad = param.grad.data
+
+        m += (1 - beta1) * (grad - m)
+        v += (1 - beta2) * (grad * grad - v)
+        param.data -= self.lr * m / (np.sqrt(v) + eps)
